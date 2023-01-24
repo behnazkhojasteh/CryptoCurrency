436e4ca96aff1bcd0fe06e53e8c571b855c78e37
Behnaz Khojasteh <bkhojaste8@gmail.com>
2023-01-24 13:38:20 -0500

    create coin property

---
+++ src/components/CoinId.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CreateButton from "./CreateButton";
+import CoinDetail from "./CoinDetail";
+import CreateChart from "./CreateChart";
+function CoinId (){
+    return (
+    <div>
+    
+        <CreateButton />
+     
+      
+        <CoinDetail />
+       
+    </div>)
+}
+export default CoinId;
